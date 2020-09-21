e88427c2173ddb47eb559db92883e6123b3c492c
Ronving <ronvings@gmail.com>
2020-09-22 00:56:15 +0300

    edit filter mapping

--- src/main/java/com/ronving/king/controller/MainController.java
+++ src/main/java/com/ronving/king/controller/MainController.java
@@ -6,6 +6,7 @@ import com.ronving.king.domain.User;
 import com.ronving.king.repos.MessageRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -27,10 +28,17 @@ public class MainController {
     }
 
     @GetMapping("/main")
-    public String main(Map<String, Object> model) {
-        Iterable<Message> messages = messageRepo.findAll();
+    public String main(@RequestParam(required = false) String filter, Model model) {
+        Iterable<Message> messages;
 
-        model.put("messages", messages);
+        if (filter != null && !filter.isEmpty()) {
+            messages = messageRepo.findByTag(filter);
+        } else {
+            messages = messageRepo.findAll();
+        }
+
+        model.addAttribute("messages", messages);
+        model.addAttribute("filter", filter);
 
         return "main";
     }
@@ -42,21 +50,6 @@ public class MainController {
         messageRepo.save(message);
 
         Iterable<Message> messages = messageRepo.findAll();
-
-        model.put("messages", messages);
-
-        return "main";
-    }
-
-    @PostMapping("filter")
-    public String filter(@RequestParam String filter, Map<String, Object> model) {
-        Iterable<Message> messages;
-
-        if (filter != null && !filter.isEmpty()) {
-            messages = messageRepo.findByTag(filter);
-        } else {
-            messages = messageRepo.findAll();
-        }
 
         model.put("messages", messages);
 
--- src/main/resources/templates/main.ftl
+++ src/main/resources/templates/main.ftl
@@ -14,9 +14,8 @@
         </form>
     </div>
     <div>Список сообщений</div>
-    <form method="post" action="filter">
-        <input type="text" name="filter">
-        <input type="hidden" name="_csrf" value="${_csrf.token}"/>
+    <form method="get" action="/main">
+        <input type="text" name="filter" value="${filter?ifExists}">
         <button type="submit">Найти</button>
     </form>
     <#list messages as message>
